9c7a232d3657ced0fb2726475879d281e9a7ded9
KateRitchie <ritchie_ecen@hotmail.com>
2023-01-14 15:37:18 -0700

    fixed tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const newTag = await Tag.create(req.body.tag_name);
+    const newTag = await Tag.create(req.body);
     res.status(200).json(newTag);
     } catch (err) {
       res.status(400).json(err)
@@ -45,7 +45,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.body.tag_name, {
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       }
